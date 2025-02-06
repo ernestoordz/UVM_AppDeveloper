5cceb3f6c8c5d1024967fe0e85ae495d93bb3223
Ernesto Ordonez <ernestoordzh@outlook.com>
2025-02-06 10:06:02 -0600

    cambios 2

---
+++ testApi.js
@@ -0,0 +1,8 @@
+const { searchCountry } = require ('./library/searchCountry.js')
+
+async function run() {
+    const countries = await searchCountry(); 
+    console.log(countries); 
+}
+  
+  run();
